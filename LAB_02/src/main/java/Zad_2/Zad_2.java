1bd1a93672859b548e4b7e9f43b6749f5aecf437
Olik <a.karasinska13@gmail.com>
2020-03-30 20:09:10 +0200

    Lab02_added

---
+++ LAB_02/src/main/java/Zad_2/Zad_2.java
@@ -0,0 +1,32 @@
+package Zad_2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.lang.Math.*;
+
+public class Zad_2 {
+    Map<Double, Double> scores = new HashMap<Double, Double>();
+    private double a = 5;
+    private double b = 5;
+    private double c = 3;
+
+    Map<Double, Double> simpleTone(double start, double stop, double ts) {
+        double A = 1.0; //[V]
+        double f = b; //[Hz]
+        double fi = c * PI; //[rad]
+        double fs = 1/ts;
+        double q=6;
+        double tmp=(2*A)/(pow(2,q));
+        double values=0;
+
+        double s;
+        for (double t = start; t <= stop; t += fs) {
+            s = A * sin(2 * PI * f * t + fi);
+            values=s/tmp;
+            double score=round(values)*tmp;
+            scores.put(t, score);
+        }
+        return scores;
+    }
+}
---
+++ LAB_02/src/main/java/Zad_2/makeChartZad2.java
@@ -0,0 +1,42 @@
+package Zad_2;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+
+public class makeChartZad2 extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Zad_2 zad_2 = new Zad_2();
+
+        //def axes
+        final NumberAxis xAxis = new NumberAxis(0, 0.2, 0.02);
+        final NumberAxis yAxis = new NumberAxis();
+
+        xAxis.setLabel("X");
+        yAxis.setLabel("Y");
+
+        //create chart
+        final LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
+        lineChart.setTitle("Simple tone");
+
+        //def series
+        XYChart.Series series = new XYChart.Series();
+
+        zad_2.simpleTone(0, 5, 2000).forEach((x, y) -> series.getData().add(new XYChart.Data(x, y)));
+
+        lineChart.getData().add(series);
+        lineChart.setCreateSymbols(false);
+        Scene scene = new Scene(lineChart, 1920, 1080);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
+ 
